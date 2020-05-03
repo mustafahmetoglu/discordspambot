a04a03b63a2d4e5944de79f3dda76381bc8cb42b
mustafa <mustafaahmetogllu@gmail.com>
2020-05-03 19:48:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,28 @@ import time
 
 client = discord.Client()
 
+
+validusers = ["mustafahmetoglu#8053","chøѕeη ✘#0001"]
+convertedArray = []
+for user in validusers:
+    newuser = user.split("#")[0]
+    convertedArray.append(newuser)
+print(convertedArray)
 @client.event
 async def on_message(message):
     x = True
-    if message.content.find("!spam aç") != -1:
-        while (x):
-            await message.channel.send("Bot çalışyo")  # If the user says !hello we will send back hi
-            time.sleep(0.3)
-
-
+        
+    if str(message.author).split("#")[0] in convertedArray :
+        if x == True:
+            if message.content.find("!haribo") != -1:
+                await message.channel.send(f"""botu çalıştıran:{message.author}""")
+                while (x == True):
+                    await message.channel.send("bot çalışıyo")  # If the user says !hello we will send back hi
+                    time.sleep(0.2)
+            elif message.content.find("!biduraq") != -1:
+                while(x):
+                    await message.channel.send("tamam gıral sakin")
+                    time.sleep(0.2)
+                    x = False
 
 client.run("token goes here",bot=False)
